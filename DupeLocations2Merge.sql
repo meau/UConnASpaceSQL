0c0f73c0480546ce1430fb93fce02647ac1385f1
Maureen Callahan <maureen.callahan@gmail.com>
2024-09-16 12:18:01 -0400

    Update DupeLocations2Merge.sql
    updated to include all duplicate entries

--- DupeLocations2Merge.sql
+++ DupeLocations2Merge.sql
@@ -1,15 +1,28 @@
 SELECT 
-    floor,
-    room,
-    area,
-    coordinate_1_label,
-    coordinate_1_indicator,
-    coordinate_2_label,
-    coordinate_2_indicator,
-    coordinate_3_label,
-    coordinate_3_indicator,
-    COUNT(*)
+    a.*
 FROM
-    uconn.location
-GROUP BY floor , room , area , coordinate_1_label , coordinate_1_indicator , coordinate_2_label , coordinate_2_indicator , coordinate_3_label , coordinate_3_indicator
-HAVING COUNT(*) > 1
+    location a
+        JOIN
+    (SELECT 
+        id,
+            floor,
+            room,
+            area,
+            coordinate_1_label,
+            coordinate_1_indicator,
+            coordinate_2_label,
+            coordinate_2_indicator,
+            coordinate_3_label,
+            coordinate_3_indicator,
+            COUNT(*)
+    FROM
+        location loc
+    GROUP BY loc.floor , loc.room , loc.area , loc.coordinate_1_label , loc.coordinate_1_indicator , loc.coordinate_2_label , loc.coordinate_2_indicator , loc.coordinate_3_label , loc.coordinate_3_indicator
+    HAVING COUNT(*) > 1) b ON a.floor = b.floor
+        AND a.coordinate_1_indicator = b.coordinate_1_indicator
+        AND a.coordinate_1_label = b.coordinate_1_label
+        AND a.coordinate_2_indicator = b.coordinate_2_indicator
+        AND a.coordinate_2_label = b.coordinate_2_label
+        AND a.coordinate_3_indicator = b.coordinate_3_indicator
+        AND a.coordinate_3_label = b.coordinate_3_label
+ORDER BY a.floor , a.room , a.area , a.coordinate_1_label , a.coordinate_1_indicator , a.coordinate_2_label , a.coordinate_2_indicator , a.coordinate_3_label , a.coordinate_3_indicator , a.create_time
